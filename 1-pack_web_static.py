86dacff22dae91b2d914ef70554993ea34e2d533
ShegzBit <feranmiawotubo16@gmail.com>
2023-12-07 23:15:02 +0100

    updated 1 and 2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,10 +6,13 @@ repo, using the function do_pack.
 """
 from fabric.api import *
 from datetime import datetime as dt
+import os
 
 # env.hosts = ['ubuntu@34.229.49.23', 'ubuntu@54.90.34.141']
 # env.user = "ubuntu"
 
+
+@task
 def do_pack():
     """
     a function that generates a .tgz archive from
@@ -20,12 +23,19 @@ def do_pack():
     day = today.day
     month = today.month
     year = today.year
-    hour =  today.hour
+    hour = today.hour
     mins = today.minute
     sec = today.second
+
     f_date = f"{year}{month}{day}{hour}{mins}{sec}"
     tar_name = f"web_static_{f_date}.tgz"
+    print(f'Packing web_static to versions/{tar_name}')
 
     local("mkdir -p versions")
-    local(f"tar -czvf versions/{tar_name} ./web_static")
-    return f"versions/{tar_name}"
+    archive = local(f"tar -czvf versions/{tar_name} ./web_static")
+    size = os.path.getsize(f"versions/{tar_name}")
+    print(f'web_static packed: verions/{tar_name} -> {size}Bytes')
+
+    if archive.succeeded:
+        return f"versions/{tar_name}"
+    return None
