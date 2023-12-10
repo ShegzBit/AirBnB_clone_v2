04038339faa970b85c06d2e7cd4be7abab4a01a4
ShegzBit <feranmiawotubo16@gmail.com>
2023-12-10 20:13:34 +0100

    solved task 2 again

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,12 +4,12 @@ a Fabric script (based on the file 1-pack_web_static.py)
 that distributes an archive to your web servers,
 using the function do_deploy:
 """
-
+from fabric.api import put, run, env
 import os
-from fabric.api import *
+
 
 env.user = "ubuntu"
-env.hosts = ['54.90.34.141', '34.229.49.23', '127.0.0.1']
+env.hosts = ['54.90.34.141', '34.229.49.23']
 
 
 def do_pack():
@@ -46,32 +46,19 @@ def do_deploy(archive_path):
     that distributes an archive to your web servers,
     using the function do_deploy:
     """
-
     if not os.path.exists(archive_path):
         return False
-
-    # versions/
-    # archive name = web_static_20170315003959
-    # .tgz
     try:
-        # upload archive
+        archive_name = os.path.basename(archive_path)
         put(archive_path, '/tmp')
-        # get filename and directory path
-        archive_name = archive_path.split('/')[-1]
-        unpacked = archive_name.split('.')[0]
-        new_dir = f'/data/web_static/releases/{unpacked}'
-        # make new dir to store unzipped archive
+        new_dir = f'/data/web_static/releases/{archive_name.strip(".tgz")}'
         run(f'mkdir -p {new_dir}')
-        # unzip archive to new dir
-        run(f'tar -xvzf /tmp/{archive_name} -C {new_dir}')
-        sudo(f'rsync -la {new_dir}/web_static/* {new_dir}')
-        # remove archive, web_static, previous sym link
-        run(f'rm /tmp/{archive_name}')
-        run(f'rm -rf {new_dir}/web_static/')
-        run('rm -rf /data/web_static/current')
-        # create new symlink
-        run(f'ln -sf {new_dir} /data/web_static/current')
-        print("New version deployed!")
+        run(f'tar -xzf /tmp/{archive_name} -C {new_dir}')
+        run(f'rsync -la {new_dir}/web_static/* {new_dir}')
+        run(f'rm -rf /tmp/{archive_name}')
+        run(f'rm -rf /data/web_static/current')
+        run(f'rm -rf {new_dir}/web_static')
+        run(f'ln -s {new_dir} /data/web_static/current')
         return True
     except Exception:
         return False
