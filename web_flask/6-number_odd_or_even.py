0c6a7d0cc9d8475650563bfe4f025362051f3f75
ShegzBit <feranmiawotubo16@gmail.com>
2023-12-25 19:08:59 +0100

    solved task 6 web flask

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -60,5 +60,13 @@ def route_number_template(n):
     return render_template('5-number.html', n=n)
 
 
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def route_odd_or_even(n):
+    """
+    prints a number template of n is odd or even
+    """
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
