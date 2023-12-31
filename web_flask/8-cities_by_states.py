387b811bdfacc0d6f309dafbeeccd8700b38282d
ShegzBit <feranmiawotubo16@gmail.com>
2023-12-31 16:24:45 +0100

    fixed output of task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -16,6 +16,9 @@ def route_cities_state():
     """
     all_states = models.storage.all(models.state.State).values()
     all_states = sorted(all_states, key=lambda state: state.name)
+    # cities = []
+    # for state in all_states:
+    #     cities.append(sorted(state.cities, key=lambda city: city.name))
     return render_template('8-cities_by_states.html', states=all_states)
 
 
